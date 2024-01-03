e16f7ff19c6fc0251b6ca5212c42ce204985a430
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-01-03 21:45:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 // your code here
 
+// let buttonEle=dcoument.getElementById("")
 
+function formSubmit() {
+	let nameInput=document.getElementById("name");
+let name=nameInput.value;
+let yearInput=document.getElementById("year");
+let year=yearInput.value;
+
+let displayUrl=document.getElementById("url");
+let urlText=displayUrl.textContent;
+	urlText=displayUrl+`${name} & ${year}`;
+
+	
+	
+	
+}
